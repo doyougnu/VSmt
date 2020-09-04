6f9327e73d420b8c18fdbc3248e2760d54f4a79e
doyougnu <jmy6342@gmail.com>
2020-09-03 17:44:46 -0700

    Simplified results api

--- haskell/vsmt/Data/Core/Result.hs
+++ haskell/vsmt/Data/Core/Result.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE LambdaCase #-}
 -----------------------------------------------------------------------------
 -- |
 -- Module    : Data.Core.Result
@@ -9,19 +10,26 @@
 -- Result module implementing variational smt models for the vsmt library
 -----------------------------------------------------------------------------
 
-{-# OPTIONS_GHC -Wall -Werror -fno-warn-orphans #-}
 {-# LANGUAGE DeriveDataTypeable #-}
 {-# LANGUAGE DeriveGeneric      #-}
 {-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE DeriveFunctor #-}
 {-# LANGUAGE RecordWildCards #-}
 {-# LANGUAGE NamedFieldPuns #-}
 {-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE RankNTypes #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE ExistentialQuantification #-}
 
 module Data.Core.Result where
 
 import           Data.Data        (Typeable)
+import           Data.Text        (Text)
 import qualified Data.SBV         as S
 import qualified Data.SBV.Control as C
+import qualified Data.SBV.Internals as I
+import qualified Data.SBV.Trans.Control as T
+import           Control.Monad.IO.Class (MonadIO)
 
 import           GHC.Generics     (Generic)
 import qualified Data.Map.Strict as M
@@ -32,11 +40,18 @@ import           Data.Core.Types
 -- | An SMT Result formula, spine strict, we leave values lazy because they may
 -- not be needed. We will be incrementally building these formulas through
 -- conses and will not be doing any other real operations until the solver is
--- finished. Thus a list is an appropriate choice
+-- finished. Thus a list is an appropriate choice. We store the variantContext
+-- with a value returned from SBV so that we can control the output to the user.
+-- You should view this as an internal datatype, in the average case this will
+-- be transformed into a Map of "variable" -> SMTLIB2 program where the SMTLIB2
+-- program will dispatch the right value based on the values of dimensions
 newtype ResultFormula a = ResultFormula [(VariantContext, a)]
-    deriving (Eq,Show,Ord,Generic,Typeable,Semigroup,Monoid)
+    deriving (Eq,Ord,Show,Generic,Typeable,Semigroup,Monoid,Functor)
 
-type VariableMap d = M.Map d (ResultFormula S.SBool)
+-- | We store the raw output from SBV (I.CV) to avoid having to use existentials
+-- and to leverage the instance already made in the SBV library. This leads to
+-- cleaner implementation on our end.
+type VariableMap d = M.Map d (ResultFormula I.CV)
 
 data Result d = Result { variables :: VariableMap d
                        , satResult :: VariantContext
@@ -58,7 +73,7 @@ onVariables f Result{..} = Result{variables=f variables, satResult}
 onSatResult :: (VariantContext -> VariantContext) -> Result d -> Result d
 onSatResult f Result{..} = Result{variables, satResult=f satResult}
 
-insertToVariables :: Ord d => d -> ResultFormula S.SBool -> Result d -> Result d
+insertToVariables :: Ord d => d -> ResultFormula I.CV -> Result d -> Result d
 insertToVariables k v = onVariables (M.insertWith mappend k v)
 
 -- | O(1) insert a result prop into the result entry for special Sat variable
@@ -76,19 +91,32 @@ isSat = do cs <- C.checkSat
 -- getResult = getResultWith . dispatchProp
 -- {-# INLINE getResult #-}
 
--- test :: IO ()
--- test = S.runSMT $
---   do x <- S.sInteger "x"
---      y <- S.sDouble "y"
---      query $
---        do S.constrain $ x .> 4
---           S.constrain $ y .< 4
---           res <- getSMTResult
---           io $ putStrLn $ show $ S.getModelDictionary res
+test :: IO ()
+test = S.runSMT $
+  do x <- S.sInteger "x"
+     y <- S.sDouble "y"
+     C.query $
+       do S.constrain $ x .> 4
+          S.constrain $ y .< 4
+          res <- C.getSMTResult
+          C.io $ print $ S.getModelDictionary res
+
+test2 :: IO ()
+test2 = S.runSMT $
+  do x <- S.sInteger "x"
+     y <- S.sDouble "y"
+     C.query $
+       do S.constrain $ x .> 4
+          S.constrain $ y .< 4
+          res <- getResult $ VariantContext $ RefB "foo"
+          let res' = variables res
+          C.io $ print $ res'
 
 -- >>> test
 -- fromList [("x",5 :: Integer),("y",5.0e-324 :: Double)]
--- <symbolic> :: SInteger
+
+-- >>> test2
+-- fromList [("x",ResultFormula [(VariantContext {getVarFormula = RefB "foo"},5 :: Integer)]),("y",ResultFormula [(VariantContext {getVarFormula = RefB "foo"},5.0e-324 :: Double)])]
 
 -- >>> :t S.ite true (S.sInt8 "x") (S.sInt8 "y")
 -- <interactive>:1:1-38: error:
@@ -125,12 +153,12 @@ isSat = do cs <- C.checkSat
 --                  deriving (Eq,Show,Generic,Semigroup,Monoid)
 
 
-getVSMTModel :: C.Query S.SMTResult
-getVSMTModel = C.getSMTResult
+getVSMTModel :: (T.MonadQuery m, MonadIO m) => m S.SMTResult
+getVSMTModel = T.getSMTResult
 
-getResult :: (Prim S.SBool a, IsString d, Ord d, S.SymVal a) =>
-  VariantContext -> a -> C.Query (Result d)
-getResult vf result =
+getResult :: (MonadIO m, T.MonadQuery m, Ord d, Show d, IsString d) =>
+  VariantContext -> m (Result d)
+getResult vf =
   do model <- getVSMTModel
      return $!
        case model of
@@ -149,6 +177,5 @@ getResult vf result =
  where
    toResMap m' =
      Result {variables = M.foldMapWithKey
-              (\k a -> M.singleton (fromString k) (makeElement a)) m'
+              (\k a -> M.singleton (fromString k) (ResultFormula $ pure (vf, a))) m'
             ,satResult=vf}
-   makeElement a = ResultFormula $ pure (vf, result .== S.fromCV a)
