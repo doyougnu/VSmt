a952d061019ba652493eeb2e5650940e845c2c96
doyougnu <jmy6342@gmail.com>
2021-03-20 16:26:42 -0700

    update gold files boolean properties implemented in test suite

--- haskell/vsmt/VSMTTestSuite/TestSuite/Solve/Laws/Boolean.hs
+++ haskell/vsmt/VSMTTestSuite/TestSuite/Solve/Laws/Boolean.hs
@@ -9,6 +9,7 @@
 -- Property based tests over logical laws which should always hold
 -----------------------------------------------------------------------------
 
+{-# OPTIONS_GHC -Wall -Werror   #-}
 {-# LANGUAGE OverloadedStrings          #-}
 {-# LANGUAGE ViewPatterns               #-}
 
@@ -24,26 +25,151 @@ properties :: TestTree
 properties =
   testGroup
     "Booleans" [ QC.testProperty "double negation is no negation" doubleNegation
+               , QC.testProperty "and is commutative"             andCommutativity
+               , QC.testProperty "or is commutative"              orCommutativity
+               , QC.testProperty "equivalence is commutative"     eqCommutativity
+               , QC.testProperty "xor is commutative"             xorCommutativity
+
+               , QC.testProperty "and is associative"             andAssociativity
+               , QC.testProperty "or is associative"              orAssociativity
+               , QC.testProperty "equivalence is associative"     eqAssociativity
+               , QC.testProperty "xor is associative"             xorAssociativity
+
+               , QC.testProperty "and annihilates"                andAnnhilates
+               , QC.testProperty "or annihilates"                 orAnnhilates
+
+               , QC.testProperty "and identity"                   andIdentity
+               , QC.testProperty "or identity"                    orIdentity
+
+               , QC.testProperty "and idempotent"                 andIdempotence
+               , QC.testProperty "or idempotent"                  orIdempotence
+               , QC.testProperty "eq idempotent"                  eqIdempotence
+
+               , QC.testProperty "and distributes over or"        andOverOr
+               , QC.testProperty "or distributes over and"        orOverAnd
+
+               , QC.testProperty "and absorbs or"                 andAbsorbsOr
+               , QC.testProperty "or absorbs and"                 orAbsorbsAnd
                ]
 
--- vsmtIsSoundBools :: OnlyBools -> QC.Property
--- vsmtIsSoundBools (unOnlyBools -> p) = noNestedChoices p && isVariational p QC.==> QCM.monadicIO $
---   do
---     liftIO $ putStrLn "--------------------- Start  -----------------------------------"
---     liftIO $ T.putStrLn $ "Proposition: " <> pretty p
---     plainRes <- liftIO $! filter sSnd <$> vOnPByConfig p
---     !res    <- liftIO $! unCounter . fSatCnt <$> solveForDiag Nothing defSettings p
---     !res2    <- liftIO $! getSatisfiableVCs <$> solve Nothing defSettings p
---     liftIO $! putStrLn $ "Variational Result: " <> show res               <> "\n"
---     liftIO $! putStrLn $ "Variational Model: " <> show (length res2)               <> "\n"
---     liftIO $! putStrLn $ "Plain       Result: " <> show (length plainRes) <> "\n"
---     liftIO $ putStrLn "--------------------- End    -----------------------------------"
---     return $ res `seq` res == length plainRes
+------------------------------ Boolean Laws ------------------------------------
+-- | shorthand type synonyms for properties over binary relations
+type UnaryProperty  = OnlyBools -> QC.Property
+
+-- | shorthand type synonyms for properties over binary relations
+type BinaryProperty  = OnlyBools -> OnlyBools -> QC.Property
+
+-- | shorthand type synonyms for properties over ternary relations
+type TernaryProperty = OnlyBools -> OnlyBools -> OnlyBools -> QC.Property
 
 
-------------------------------- Boolean Laws -----------------------------------
-doubleNegation :: OnlyBools -> QC.Property
+---------------------------- Special Cases -------------------------------------
+andAnnhilates :: UnaryProperty
+andAnnhilates (unOnlyBools -> p) = QCM.monadicIO $
+  do left  <- liftIO $ unCounter . fSatCnt <$> solveForDiagnostics Nothing defSettings (p &&& false)
+     right <- liftIO $ unCounter . fSatCnt <$> solveForDiagnostics Nothing defSettings false
+     return $! left == right
+
+orAnnhilates :: UnaryProperty
+orAnnhilates (unOnlyBools -> p) = QCM.monadicIO $
+  do left  <- liftIO $ unCounter . fSatCnt <$> solveForDiagnostics Nothing defSettings (p ||| true)
+     right <- liftIO $ unCounter . fSatCnt <$> solveForDiagnostics Nothing defSettings true
+     return $! left == right
+
+andIdentity :: UnaryProperty
+andIdentity (unOnlyBools -> p) = QCM.monadicIO $
+  do left  <- liftIO $ unCounter . fSatCnt <$> solveForDiagnostics Nothing defSettings (p &&& true)
+     right <- liftIO $ unCounter . fSatCnt <$> solveForDiagnostics Nothing defSettings p
+     return $! left == right
+
+orIdentity :: UnaryProperty
+orIdentity (unOnlyBools -> p) = QCM.monadicIO $
+  do left  <- liftIO $ unCounter . fSatCnt <$> solveForDiagnostics Nothing defSettings (p ||| false)
+     right <- liftIO $ unCounter . fSatCnt <$> solveForDiagnostics Nothing defSettings p
+     return $! left == right
+
+andOverOr :: TernaryProperty
+andOverOr (unOnlyBools -> p) (unOnlyBools -> q) (unOnlyBools -> r) = QCM.monadicIO $
+  do left  <- liftIO $ unCounter . fSatCnt <$> solveForDiagnostics Nothing defSettings (p &&& (q ||| r))
+     right <- liftIO $ unCounter . fSatCnt <$> solveForDiagnostics Nothing defSettings ((p &&& q) ||| (p &&& r))
+     return $! left == right
+
+orOverAnd :: TernaryProperty
+orOverAnd (unOnlyBools -> p) (unOnlyBools -> q) (unOnlyBools -> r) = QCM.monadicIO $
+  do left  <- liftIO $ unCounter . fSatCnt <$> solveForDiagnostics Nothing defSettings (p ||| (q &&& r))
+     right <- liftIO $ unCounter . fSatCnt <$> solveForDiagnostics Nothing defSettings ((p ||| q) &&& (p ||| r))
+     return $! left == right
+
+
+andAbsorbsOr :: BinaryProperty
+andAbsorbsOr (unOnlyBools -> p) (unOnlyBools -> q) = QCM.monadicIO $
+  do left  <- liftIO $ unCounter . fSatCnt <$> solveForDiagnostics Nothing defSettings (p &&& (p ||| q))
+     right <- liftIO $ unCounter . fSatCnt <$> solveForDiagnostics Nothing defSettings p
+     return $! left == right
+
+orAbsorbsAnd :: BinaryProperty
+orAbsorbsAnd (unOnlyBools -> p) (unOnlyBools -> q) = QCM.monadicIO $
+  do left  <- liftIO $ unCounter . fSatCnt <$> solveForDiagnostics Nothing defSettings (p ||| (p &&& q))
+     right <- liftIO $ unCounter . fSatCnt <$> solveForDiagnostics Nothing defSettings p
+     return $! left == right
+
+---------------------------- General Cases -------------------------------------
+doubleNegation :: UnaryProperty
 doubleNegation (unOnlyBools -> p) = QCM.monadicIO $
   do left  <- liftIO $ unCounter . fSatCnt <$> solveForDiagnostics Nothing defSettings p
      right <- liftIO $ unCounter . fSatCnt <$> solveForDiagnostics Nothing defSettings (bnot $ bnot p)
      return $! left == right
+
+commutes :: (Proposition -> Proposition -> Proposition) -> BinaryProperty
+commutes operator (unOnlyBools -> p) (unOnlyBools -> q) = QCM.monadicIO $
+  do left  <- liftIO $ unCounter . fSatCnt <$> solveForDiagnostics Nothing defSettings (p `operator` q)
+     right <- liftIO $ unCounter . fSatCnt <$> solveForDiagnostics Nothing defSettings (q `operator` p)
+     return $! left == right
+
+associates :: (Proposition -> Proposition -> Proposition) -> TernaryProperty
+associates operator (unOnlyBools -> p) (unOnlyBools -> q) (unOnlyBools -> r) = QCM.monadicIO $
+  do left  <- liftIO $ unCounter . fSatCnt <$> solveForDiagnostics Nothing defSettings ((p `operator` q) `operator` r)
+     right <- liftIO $ unCounter . fSatCnt <$> solveForDiagnostics Nothing defSettings (p `operator` (q `operator` r))
+     return $! left == right
+
+idempotence :: (Proposition -> Proposition -> Proposition) -> UnaryProperty
+idempotence operator (unOnlyBools -> p) = QCM.monadicIO $
+  do left  <- liftIO $ unCounter . fSatCnt <$> solveForDiagnostics Nothing defSettings (p `operator` p)
+     right <- liftIO $ unCounter . fSatCnt <$> solveForDiagnostics Nothing defSettings p
+     return $! left == right
+
+
+andCommutativity :: BinaryProperty
+andCommutativity = commutes (&&&)
+
+orCommutativity :: BinaryProperty
+orCommutativity = commutes (|||)
+
+eqCommutativity :: BinaryProperty
+eqCommutativity = commutes (<=>)
+
+xorCommutativity :: BinaryProperty
+xorCommutativity = commutes (<+>)
+
+
+andAssociativity :: TernaryProperty
+andAssociativity = associates (&&&)
+
+orAssociativity :: TernaryProperty
+orAssociativity  = associates (|||)
+
+eqAssociativity :: TernaryProperty
+eqAssociativity = associates (<=>)
+
+xorAssociativity :: TernaryProperty
+xorAssociativity = associates (<+>)
+
+
+andIdempotence :: UnaryProperty
+andIdempotence = idempotence (&&&)
+
+orIdempotence :: UnaryProperty
+orIdempotence = idempotence (|||)
+
+eqIdempotence :: UnaryProperty
+eqIdempotence = idempotence (<=>)
