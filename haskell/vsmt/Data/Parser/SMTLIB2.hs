5ce6e27a044acb153424dd498a0b219d1004d844
doyougnu <jmy6342@gmail.com>
2021-03-28 17:42:24 -0700

    lang parser monad and file

---
+++ haskell/vsmt/Data/Parser/SMTLIB2.hs
@@ -0,0 +1,59 @@
+-----------------------------------------------------------------------------
+-- |
+-- Module    : Parser.SMTLIB2
+-- Copyright : (c) Jeffrey Young
+-- License   : BSD3
+-- Maintainer: youngjef@oregonstate.edu
+-- Stability : experimental
+--
+-- Parser for the core variational propositional logic language should be as
+-- identical as possible to SMTLIB2 except should not expose push/pop
+-----------------------------------------------------------------------------
+
+{-# OPTIONS_GHC -Wall -Werror #-}
+
+{-# LANGUAGE DeriveGeneric              #-}
+{-# LANGUAGE DerivingStrategies         #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE OverloadedStrings          #-}
+{-# LANGUAGE TupleSections              #-}
+
+module Parser.SMTLIB2 where
+
+import           Control.Applicative (Alternative)
+import           Control.Monad.Combinators.Expr
+
+import qualified Control.Monad.State.Strict     as S
+
+import           Data.Functor                   (void)
+import qualified Data.Text                      as T
+import           Data.Text.IO                   (readFile)
+import           Data.Void                      (Void)
+import           Prelude                        hiding (EQ, GT, LT, readFile)
+import           Text.Megaparsec
+import qualified Text.Megaparsec.Char           as C
+import qualified Text.Megaparsec.Char.Lexer     as L
+
+
+import           Core.Types
+
+-- | Meta data tracked during parsing
+data MetaData = MetaData { variableCnt  :: Int -- ^ the number of variables
+                         , choiceCnt    :: Int -- ^ the number of choices
+                         , dimensionCnt :: Int -- ^ the number of unique dimensions
+                         }
+
+-- | the parser monad
+newtype Parser a = Parser { runParser :: S.StateT MetaData (Parsec Void T.Text) a }
+  deriving newtype (Functor,Applicative,Alternative,Monad  -- base
+                    , S.MonadPlus, MonadParsec Void T.Text -- required for MonadParsec
+                    , S.MonadState MetaData                -- and the state monad of course
+                    )
+
+-- parseFromFile ::
+--      FilePath
+--   -> IO (Either (ParseErrorBundle T.Text Void) (VariantContext, CheckableResult))
+-- parseFromFile f = parse parser f <$> readFile f
+
+-- parser :: Parser (VariantContext, CheckableResult)
+-- parser = between sc eof go
